a0d9564471018138f8d2ce23e45bf66d94b5f0be
Gvidas <gvisis@gmail.com>
2021-03-01 01:51:09 +0200

    updated paths

---
+++ 02_better/public/products/create.php
@@ -0,0 +1,43 @@
+<?php
+require_once "../../database.php";
+require_once "../../functions.php";
+
+// errors turi but virsuje, nes kitaip nemato kai tuscias create.php
+$errors = [];
+
+// issirasom situos i virsu, kad refreshinus atsimintu puslapis kas buvo ivesta jeigu yra klaida
+$title = '';
+$price = '';
+$description = '';
+$product = [
+  'image' => ''
+];
+// We need to check if method is POST only then we add something to DB 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  require_once "../../validate_product.php";
+  if (empty($errrors)) {
+    $statemenet = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                  VALUES (:title, :image, :description, :price, :date)");
+
+    //TODO: we bind :title to my variable $title;
+    $statemenet->bindValue(':title', $title);
+    $statemenet->bindValue(':image', $imagePath);
+    $statemenet->bindValue(':description', $description);
+    $statemenet->bindValue(':price', $price);
+    $statemenet->bindValue(':date', date('Y-m-d H:i:s'));
+
+    // After everything
+    $statemenet->execute(); // makes a change in the database;
+    header('Location: index.php');
+    exit;
+  }
+}
+?>
+<?php include_once "../../views/partials/header.php"; ?>
+<h1>Create new product</h1>
+
+<?php include_once "../../form.php" ?>
+
+</body>
+
+</html>
---
+++ 02_better/public/products/update.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once "../../database.php";
+require_once "../../functions.php";
+
+$id = $_GET['id'] ?? null;
+if (!$id) {
+  header("Location: index.php");
+  exit;
+}
+
+// Select specific product from DB
+$statement = $pdo->prepare('SELECT * FROM products WHERE id = :id');
+$statement->bindValue(':id', $id);
+$statement->execute();
+$product = $statement->fetch(PDO::FETCH_ASSOC);
+
+$errors = [];
+
+$title = $product['title'];
+$price = $product['price'];
+$description = $product['description'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  require_once "../../validate_product.php";
+  
+  if (empty($errors)) {
+   
+    $statemenet = $pdo->prepare("UPDATE products SET title = :title, image = :image, description = :description, price = :price WHERE id = :id");
+
+    //TODO: we bind :title to my variable $title;
+    $statemenet->bindValue(':title', $title);
+    $statemenet->bindValue(':image', $imagePath);
+    $statemenet->bindValue(':description', $description);
+    $statemenet->bindValue(':price', $price);
+    $statemenet->bindValue(':id', $id);
+
+    // After everything
+    $statemenet->execute(); // makes a change in the database;
+    header('Location: index.php');
+    exit;
+  }
+}
+
+?>
+<?php include_once "../../views/partials/header.php"; ?>
+
+<h1>Update item: <?= $product['title'] ?></h1>
+
+<?php include_once "../../form.php" ?>
+
+</body>
+
+</html>
